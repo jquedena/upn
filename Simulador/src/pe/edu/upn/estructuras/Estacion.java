0931cb065afd3e1e6d68f0d02e802adf4ed54c30
Estudiante <Estudiante@UPNLNLAB1318>
2017-05-16 21:01:33 -0500

    Base de datos

---
+++ Simulador/src/pe/edu/upn/estructuras/Estacion.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.upn.estructuras;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Estacion {
+    
+    private String nombre;
+    private int distancia;
+    private Unidad unidad;
+    private String sentido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public Unidad getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(Unidad unidad) {
+        this.unidad = unidad;
+    }
+
+    public String getSentido() {
+        return sentido;
+    }
+
+    public void setSentido(String sentido) {
+        this.sentido = sentido;
+    }
+    
+}
