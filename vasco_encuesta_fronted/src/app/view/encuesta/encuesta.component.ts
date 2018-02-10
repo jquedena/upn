2da6c1a980335d330ef812ffe6ef324eb2fe3102
Juan Quedena <juanquedena@gmail.com>
2018-02-10 18:17:59 -0500

    Diseño de menu

--- vasco_encuesta_fronted/src/app/view/encuesta/encuesta.component.ts
+++ vasco_encuesta_fronted/src/app/view/encuesta/encuesta.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class EncuestaComponent implements OnInit {
 
   isNavbarCollapsed: boolean;
+  isToogle: boolean = false;
+  hideText: string = "nav-show";
+  classButtonToggle: string = "fa fa-fw fa-angle-left";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickToggleMenu():void {
+    this.isToogle = !this.isToogle;
+    this.hideText = this.isToogle ? "nav-hidden" : "nav-show";
+    this.classButtonToggle = this.isToogle ? "fa fa-fw fa-angle-right" : "fa fa-fw fa-angle-left";
+  }
 }
